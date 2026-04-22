5293ffa8fdd55212217e57d108d0265a98c84c47
Samiran Banerjee <bsamibane786@gmail.com>
2026-04-22 16:40:55 +0530

    Dynamic length array creation example

---
+++ Array-Fundamentals/JS_P2_DynamicLength_ArrayCreation.js
@@ -0,0 +1,9 @@
+// 1 way - by creating new Array OBJECT
+let marks = new Array (12,13,11,10,15);
+console.log(marks.length);//5
+console.log(marks);//5
+
+// 2 way - directly creting array - 
+let newMarks = [9,5,6,8];
+console.log(newMarks.length);
+console.log(newMarks);
