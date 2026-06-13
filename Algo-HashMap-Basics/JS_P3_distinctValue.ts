92fa7baa849bfd03093c29794012d8ba96583903
Samiran Banerjee <bsamibane786@gmail.com>
2026-06-13 23:40:13 +0530

    HashSet - distinct value

--- Algo-HashMap-Basics/JS_P3_distinctValue.ts
+++ Algo-HashMap-Basics/JS_P3_distinctValue.ts
@@ -2,7 +2,7 @@
 Given an array arr[] of length N, The task is to count all distinct elements in arr[].
 arr[6] = [10, 20, 20, 10, 30, 10]
 */
-
+// Way -1
 let disArr: number[] = [10, 20, 20, 10, 30, 10];
 let disHashMap = new Map<number,number>();
 for(let k of disArr){
@@ -17,3 +17,9 @@ for(let k of disHashMap.keys()){
 console.log(cont);
 
 // Needs to refine this code with Set concept, also need to implement size method of Set/Hashmap
+
+//Way 2 - 
+let disArrMap = new Set<number>(disArr);
+console.log(disArrMap.size);
+// console.log([...disArrMap]);
+// console.log(disArrMap.delete(20));
