f367e1b84a4c4cf9fa38eb8a95a2489c833a8221
Samiran Banerjee <bsamibane786@gmail.com>
2026-07-01 02:14:42 +0530

    Finding the number of characters in each word of given string by Map

---
+++ Problem-Solving/JS_P4_charCountFromString.ts
@@ -0,0 +1,13 @@
+let charCount:string = 'The String Input will be fun';
+let charCountArr:string[] = charCount.split(/\s+/);
+console.log(charCountArr);
+let count:number=0;
+let charCountMap = new Map<string,number>();
+for(let i=0; i<charCountArr.length;i++){
+    count = charCountArr[i].length;
+    if(!charCountMap.has(charCountArr[i])){
+        charCountMap.set(charCountArr[i],count);
+    }
+}
+console.log(charCountMap);
+
