f988d9595003283d1c08b515a0b232e2fd88f22e
Samiran Banerjee <bsamibane786@gmail.com>
2026-08-24 23:32:48 +0530

    TS - P9 - Array-2 - Maximum Bounded  - O(N) - optimized

--- AlgoPrep/Array/Algo-Array-Advanced/Array-2/TS-P9-maxBoundedOpt.ts
+++ AlgoPrep/Array/Algo-Array-Advanced/Array-2/TS-P9-maxBoundedOpt.ts
@@ -1,6 +1,6 @@
 /*
 ==================================================================================================================
-Arrays2_BoundedMaximum - O(N^2) Time Complexity
+Arrays2_BoundedMaximum - O(N) Time Complexity
 Success rate: 32.96%
 
 Given an integer array nums and two integers left and right, return the number of contiguous non-empty subarrays such that the value of the maximum array element in that subarray is in the range [left, right].
