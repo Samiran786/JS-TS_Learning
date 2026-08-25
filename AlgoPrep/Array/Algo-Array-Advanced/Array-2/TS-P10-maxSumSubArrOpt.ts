ddc0c2a7926ca03947924e94b04ae45fc8024f82
Samiran Banerjee <bsamibane786@gmail.com>
2026-08-26 01:18:43 +0530

    TS - P11 - Array-2 - Kadan's Algo

---
+++ AlgoPrep/Array/Algo-Array-Advanced/Array-2/TS-P10-maxSumSubArrOpt.ts
@@ -0,0 +1,58 @@
+/*
+=====================================================================================
+Arrays2_Max Sum Subarray
+Success rate: 46.39%
+
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Input Format:
+The first line contains an integer n (length of the array nums).
+The second line contains n space-separated integers representing the elements of the array nums.
+
+Output Format:
+A single integer representing the sum of the subarray with the largest sum.
+
+Constraints:
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+
+Sample test cases
+Input
+9
+-2 1 -3 4 -1 2 1 -5 4
+
+Expected output
+
+6
+=============================================================================================
+ */
+
+let arr_maxSub:number[] = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+let arr_maxSub1:number[] = [3,4,2,-14,16,-20,5];
+
+function maxSumSubArr(arr:number[]):number{
+    let currentPrefSum:number = 0;
+    let minimumPrefSum:number = 0;
+    let candidate:number = 0;
+    let ans:number = 0;
+    for(let i=0; i<arr.length; i++){
+        if(i===0){
+            currentPrefSum = arr[0];
+            minimumPrefSum = arr[0];
+            candidate = arr[0];
+            ans = candidate;
+        }else{
+            currentPrefSum += arr[i];
+            candidate = currentPrefSum - minimumPrefSum;
+            (currentPrefSum < minimumPrefSum) ? minimumPrefSum = currentPrefSum : minimumPrefSum ; 
+        }
+        if(candidate>ans){
+            ans = candidate
+        }
+    }
+    return ans;
+}
+
+console.log(maxSumSubArr(arr_maxSub));
+console.log(maxSumSubArr(arr_maxSub1));
+
--- AlgoPrep/Array/Algo-Array-Advanced/Array-2/TS-P11-maxSumSubArrKadan.ts
+++ AlgoPrep/Array/Algo-Array-Advanced/Array-2/TS-P11-maxSumSubArrKadan.ts
@@ -1,6 +1,6 @@
 /*
 =====================================================================================
-Arrays2_Max Sum Subarray
+Arrays2_Max Sum Subarray - O(n) - kadan's Algorithm
 Success rate: 46.39%
 
 Given an integer array nums, find the subarray with the largest sum, and return its sum.
@@ -27,30 +27,25 @@ Expected output
 =============================================================================================
  */
 
-let arr_maxSub:number[] = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+let arr_kad:number[] = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-function maxSumSubArr(arr:number[]):number{
-    let currentPrefSum:number = 0;
-    let minimumPrefSum:number = 0;
-    let candidate:number = 0;
-    let ans:number = 0;
+function maxSumKad(arr:number[]):number{
+    let currentVal:number = 0;
+    let sum:number = 0;
+    let ans:number = -Infinity;
+
     for(let i=0; i<arr.length; i++){
-        if(i===0){
-            currentPrefSum = arr[0];
-            minimumPrefSum = arr[0];
-            candidate = arr[0];
-            ans = candidate;
+        currentVal = arr[i];
+        if(sum>=0){
+            sum += arr[i];
         }else{
-            currentPrefSum += arr[i];
-            candidate = currentPrefSum - minimumPrefSum;
-            (currentPrefSum < minimumPrefSum) ? minimumPrefSum = currentPrefSum : minimumPrefSum ; 
+            sum = arr[i];
         }
-        if(candidate>ans){
-            ans = candidate
+        if(sum>ans){
+            ans = sum;
         }
     }
     return ans;
 }
 
-console.log(maxSumSubArr(arr_maxSub));
-
+console.log(maxSumKad(arr_kad));
